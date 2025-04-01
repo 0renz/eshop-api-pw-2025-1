5546b46715e5cd5c82cb8e4cb98be210706425bb
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2025-04-01 10:30:04 -0300

    Adiciona backend CRUD para produtos

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const { rotasCategorias } = require('./rotasCategorias');
+const { rotasProdutos } = require('./rotasProdutos');
 
 const rotas = new Router();
 
-rotas.use(rotasCategorias);
+rotas.use(rotasCategorias, rotasProdutos);
 
 module.exports = { rotas };
