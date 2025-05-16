2070d7f16a8f3fa821e713689c615cd3c5b03bab
Lorenzo Schauenberg <lorenzoschauenberg@gmail.com>
2025-05-16 10:19:33 -0300

    feat: Adiciona camada de segurança

--- routes/rotas.js
+++ routes/rotas.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
+
 const { rotasCategorias } = require('./rotasCategorias');
 const { rotasProdutos } = require('./rotasProdutos');
+const { login } = require('../controllers/segurancaController');
 
 const rotas = new Router();
 
-rotas.use(rotasCategorias, rotasProdutos);
+rotas.use(rotasCategorias);
+rotas.use(rotasProdutos);
+
+// rota para fazer o login e pegar o JWT
+rotas.route("/login")
+   .post(login)           
+	 
 
 module.exports = { rotas };
